fb9548285cdaa4a45d4cf1eec13b57d9a63751e0
CSteinmuller <chris.dungeonmaster@gmail.com>
2017-08-27 13:35:14 -0500

    Added rudimentry player functions/class

--- js/terminal.js
+++ js/terminal.js
@@ -10,3 +10,56 @@ function output (input, flag) {
     var outputElement = document.getElementById ('output-container');
     outputElement.innerHTML += outputText;
 }
+
+
+class Player
+{
+    constructor (cname)
+    {
+    this.name = name;
+    this.level = 1;
+    this.xp = 0;
+    this.maxhp = 10;
+    this.hp = maxhp;
+    this.maxmana = 5
+    this.mana = maxmana;
+    this.weapon = "sword";
+    this.damage = 6;
+    this.spell = "power bolt";
+    this.spelldam =  4;
+    this.healpot = 1;
+    this.manapot = 0;
+    }
+    HealMe()
+    {
+        if (this.healpot > 0)
+        {
+            this.hp = this.maxhp;
+            this.healpot--;
+        }
+    }
+    RestoreMana()
+    {
+        if (this.manapot > 0)
+        {
+            this.mana = this.maxmana;
+            this.manapot--;
+        }
+    } 
+    AddXp(sweetloot)
+    {
+        this.xp += sweetloot;
+        if (this.xp >= this.level * 1000) 
+        {
+            LevelUp();
+        }
+    }
+    LevelUp()
+    {
+    this.level++;
+    this.xp = 0;
+    this.maxhp += 5;
+    this.maxmana += 3;
+    }
+    
+}
