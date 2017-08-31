88122bd4559e47a6d865d8c89027d96b1ff90b7d
Mackenzie <mluzarow@users.noreply.github.com>
2017-08-31 07:59:54 -0500

    Added a maptile class to hold tile info

--- js/gridder.js
+++ js/gridder.js
@@ -1,4 +1,14 @@
-function gridify () {
+/**
+* Using a .map file, renders entire map to screen
+*
+* @uses parseMap() to parse map file into MapTile objects
+*
+* @param  string  mapName  the name of the map file that is to be rendered
+*/
+function gridify (mapName) {
+    // Parse map as list of MapTile objects
+    //var boxList = parseMap (mapName);
+    
     document.getElementById('draw-space').innerHTML += drawBox (0, '../data/map/tile/test', 1, 40);
 }
 
@@ -10,4 +20,12 @@ function drawBox (index, tilePath, boardWidth, boxSize) {
        'background-image: url(\'' + tilePath + '.png\'); "';
 
     return '<div class = "grid_box" ' + style + '></div>';
+}
+
+class MapTile {
+    constructor (index, showFlag=true, tileBG=null) {
+        this.index = index;
+        this.showFlag = showFlag;
+        this.tileBG = tileBG;
+    }
 }
