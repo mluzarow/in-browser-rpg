898ba13b8659ec6c4d544ba1372793725e94cc65
Mackenzie <ma.luzarow@gmail.com>
2017-08-31 20:24:27 -0500

    Gridifier now uses map data.

--- js/gridder.js
+++ js/gridder.js
@@ -1,31 +1,41 @@
 /**
-* Using a .map file, renders entire map to screen
+* Using a js map file (maps.js), renders entire map to screen.
 *
-* @uses parseMap() to parse map file into MapTile objects
+* @uses  drawBox()      Renders individual boxes to the play area.
+* @uses  maps.js->maps  Maps content array holding all map data.
 *
-* @param  string  mapName  the name of the map file that is to be rendered
+* @param  {string}  mapName  The name of the map file that is to be rendered.
 */
 function gridify (mapName) {
-    // Parse map as list of MapTile objects
-    //var boxList = parseMap (mapName);
-    
-    document.getElementById('draw-space').innerHTML += drawBox (0, '../data/map/tile/test', 1, 40);
+    var drawSpace = document.getElementById('draw-space');
+    var boardWidth = maps[mapName]['width'];
+
+    for (var box in maps[mapName]['data']) {
+        drawSpace.innerHTML += drawBox (box, box['bg'], boardWidth, 40);
+    }
 }
 
+/**
+ * Draws a tile box to the play area.
+ *
+ * @param  {int}     index       The index of the box that is currently being
+ *                               rendered. The index is linear, starting from 0
+ *                               for the first value on the top left and continuing
+ *                               from left to right, top to bottom, until reaching
+ *                               the bottom right.
+ * @param  {string}  tilePath    The path of the tile background resource that is
+ *                               to be rendered with the tile.
+ * @param  {int}     boardWidth  The width (in tiles) of the current map.
+ * @param  {int}     boxSize     The width of the square tiles used in this map.
+ *
+ * @return  {string}  The HTML output of the constructed tile box.
+ */
 function drawBox (index, tilePath, boardWidth, boxSize) {
     var style =
         'style = "' +
         'top: ' + (boxSize * ~~(index / boardWidth)) + 'px; ' +
         'left: ' + (boxSize * (index % boardWidth)) + 'px; ' +
-       'background-image: url(\'' + tilePath + '.png\'); "';
+        'background-image: url(\'' + tilePath + '.png\'); "';
 
     return '<div class = "grid_box" ' + style + '></div>';
-}
-
-class MapTile {
-    constructor (index, showFlag=true, tileBG=null) {
-        this.index = index;
-        this.showFlag = showFlag;
-        this.tileBG = tileBG;
-    }
 }
---
+++ js/maps.js
@@ -0,0 +1,31 @@
+var maps = {
+    'testMap1' : {
+        'width' : '3',
+        'data' : {
+            0 : {
+                'render' : true,
+                'bg' : 'red'
+            },
+            1 : {
+                'render' : true,
+                'bg' : 'red'
+            },
+            2 : {
+                'render' : true,
+                'bg' : 'red'
+            },
+            3 : {
+                'render' : true,
+                'bg' : 'red'
+            },
+            4 : {
+                'render' : true,
+                'bg' : 'red'
+            },
+            5 : {
+                'render' : true,
+                'bg' : 'blue'
+            }
+        }
+    }
+}
